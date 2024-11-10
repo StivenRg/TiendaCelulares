4dbd825e76c951e95b85d3b68aef63ccb39d1de2
Stiven Dev <stiven.rico1407@gmail.com>
2024-11-10 13:27:35 -0500

    Update Inventario

--- src/co/edu/uptc/negocio/Inventario.java
+++ src/co/edu/uptc/negocio/Inventario.java
@@ -5,13 +5,13 @@ import co.edu.uptc.modelo.Producto;
 import java.util.ArrayList;
 
 public class Inventario{
-	private static       int                  totalCantidad;
-	private static       double               totalPrecioBase;
-	private static       double               totalPrecioVenta;
-	private static       double               totalImpuesto;
-	private static       double               totalComision;
-	private static       double               totalGanancia;
-	private static final ArrayList <Producto> productos = new ArrayList <>();
+	private static       int                  totalCantidad    = 0;
+	private static       double               totalPrecioBase  = 0;
+	private static       double               totalPrecioVenta = 0;
+	private static       double               totalImpuesto    = 0;
+	private static       double               totalComision    = 0;
+	private static       double               totalGanancia    = 0;
+	private static final ArrayList <Producto> productos        = new ArrayList <>();
 
 	public Inventario (int paramTotalCantidad, double paramTotalPrecioBase, double paramTotalPrecioVenta, double paramTotalImpuesto, double paramTotalComision, double paramTotalGanancia){
 		totalCantidad    = paramTotalCantidad;
@@ -26,29 +26,59 @@ public class Inventario{
 	}
 
 	public static void agregarProducto (Producto producto){
+		if (producto.getCantidad() <= 0){
+			return;
+		}
+		for (Producto locProducto : productos){
+			if (locProducto.getCodigo().equals(producto.getCodigo())){
+				locProducto.setCantidad(locProducto.getCantidad() + producto.getCantidad());
+				return;
+			}
+		}
 		productos.add(producto);
 	}
 
-	public static void descontarProducto (String paramCodigoProducto, int paramCantidad){
-		for (Producto producto : productos){
-			if (producto.getCodigo().equals(paramCodigoProducto)){
-				producto.setCantidad(producto.getCantidad() - paramCantidad);
-			}
-		}
-	}
-
-	public static void calcularTotalCantidad (){
+	private static void calcularTotalCantidad (){
+		totalCantidad = 0;
 		for (Producto producto : productos){
 			totalCantidad += producto.getCantidad();
 		}
 	}
 
-	public static double calcularTotalPrecioBase (){
+	private static void calcularTotalPrecioBase (){
+		totalPrecioBase = 0;
 		for (Producto producto : productos){
-			if (producto.getPrecio() > 0){
-				totalPrecioBase += producto.getPrecio() * producto.getCantidad();
+			if (producto.getCantidad() > 0){
+				totalPrecioBase += (producto.getPrecio() * producto.getCantidad());
 			}
 		}
-		return totalPrecioBase;
+	}
+
+	private static void calcularTotalPrecioVenta (){
+		totalPrecioVenta = totalPrecioBase + totalImpuesto;
+	}
+
+	private static void calcularTotalImpuesto (){
+		totalImpuesto = 0;
+		for (Producto producto : productos){
+			if (producto.getPrecio() > 600000){
+				totalImpuesto += producto.getPrecio() * 19 / 100;
+			} else{
+				totalImpuesto += producto.getPrecio() * 5 / 100;
+			}
+		}
+	}
+
+	private static void calcularTotalComision (){
+		totalComision = 0;
+		for (Producto producto : productos){
+			if (producto.getCantidad() > 0){
+				totalComision += producto.getPrecio() * producto.getCantidad() * 0.05;
+			}
+		}
+	}
+
+	private static void calcularTotalGanancia (){
+		totalGanancia = totalPrecioBase - totalComision;
 	}
 }
